63e72bcbb1f30427c2ea5c7de70ac97325c7be6f
Emma Paris <eparis@atlassian.com>
2020-05-15 22:04:33 +0200

    scene builder error \

---
+++ src/application/Controller.java
@@ -0,0 +1,12 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class Controller {
+	
+	@FXML
+	private Button fs; 
+
+}
