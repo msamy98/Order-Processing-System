f1e5da6d9af83acc9d65b6a209a580041f4b312e
Mohamed Samy <mohamedsamyi9100@gmail.com>
2020-05-16 03:43:14 +0200

    some printing setup

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,6 @@
 package application;
 	
 import java.sql.*;
-
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -24,24 +22,36 @@ public class Main extends Application {
 			
 			Database bookstore = new Database() ; 
 			bookstore.databaseConnector(); 
-			//bookstore.setQuery("INSERT INTO order_processing_system.publisher VALUES(\'Ahmed\', \'Zecola\', \'012-27-11-14-2\');");
+			
+			
+			bookstore.setQuery("INSERT INTO order_processing_system.publisher VALUES(\'Ziad Taha\', \'Ard El Abtal\', \'011-5537-6877\');");
+			bookstore.excuteUpdateQuery();
+			//bookstore.setQuery("DELETE FROM order_processing_system.publisher WHERE PUBLISHER_NAME = \'Ziad Taha\';");
 			//bookstore.excuteUpdateQuery();
 			bookstore.setQuery("SELECT * FROM order_processing_system.publisher;");
 			ResultSet result = bookstore.executeRetriveQuery();
 			ResultSetMetaData metaData = result.getMetaData();
+			
+			
 			while(result.next()) {
 				StringBuilder temp = new StringBuilder();
+				
 				for (int i = 1 ; i<=metaData.getColumnCount() ; i++) {
 					if (i>1) {
-						temp.append(" , ");
+						temp.append("\n");
 					}
+					temp.append(metaData.getColumnName(i)).append(": ");
 					temp.append("\"");
 					temp.append(result.getString(i));
 					temp.append("\"");
 				}
 				System.out.println(temp.toString());
+				System.out.println("-----------------------------");
 			}
+			
 			result.close();
+			
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
